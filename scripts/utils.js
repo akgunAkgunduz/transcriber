284f9ce74730c872ef70e45294531e83e39a1197
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-08-07 22:59:13 +0300

    Update utilities

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,15 +1,25 @@
 const supportedFileTypes = ['mp3']
 
-const sanitizeFilePath = (path) => path.replace('#', '%23')
+const sanitizeFilePath = (path) => {
+  return path.replace('#', '%23')
+}
 
 const generateDurationText = (seconds) => {
-  const string = new Date(seconds * 1000).toISOString()    
-  return seconds < 3600 ? string.substr(14, 5) : string.substr(11, 8)
+  const string = new Date(seconds * 1000).toISOString()
+  const result = seconds < 3600 ? string.substr(14, 5) : string.substr(11, 8)
+  
+  return result
 }
 
 const generatePositionText = (seconds, duration) => {
-  const string = new Date(seconds * 1000).toISOString()    
-  return duration < 3600 ? string.substr(14, 5) : string.substr(11, 8)
+  if (!duration) {
+    return ''
+  }
+  
+  const string = new Date(seconds * 1000).toISOString()
+  const result = duration < 3600 ? string.substr(14, 5) : string.substr(11, 8)
+
+  return result
 }
 
 module.exports = {
