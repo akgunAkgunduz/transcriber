e91bb45bbe240a472e12cb83794463391a43a071
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-08-16 12:18:30 +0300

    Add user interface elements module

---
+++ utils/userInterfaceElements.js
@@ -0,0 +1,21 @@
+module.exports = {
+  app: document.getElementById('app'),
+  audio: document.getElementById('audio'),
+  fileDiv: document.getElementById('file-div'),
+  toStart: document.getElementById('to-start'),
+  rewind: document.getElementById('rewind'),
+  playPause: document.getElementById('play-pause'),
+  forward: document.getElementById('forward'),
+  repeat: document.getElementById('repeat'),
+  progressAndPosition: document.getElementById('progress-and-position'),
+  progressDiv: document.getElementById('progress-div'),
+  progress: document.getElementById('progress'),
+  fakeProgress: document.getElementById('fake-progress'),
+  progressInfo: document.getElementById('progress-info'),
+  songLength: document.getElementById('song-length'),
+  songPosition: document.getElementById('song-position'),
+  songVolume: document.getElementById('song-volume'),
+  songRate: document.getElementById('song-rate'),
+  volume: document.getElementById('volume'),
+  speed: document.getElementById('speed')
+}
